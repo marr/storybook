b09aff60d61a1b210fdd22a4eb8a74c56da1a58f
Dave Marr <dave@turo.com>
2019-01-24 13:45:08 -0800

    Use exportTo for :root definitions

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ const customProperties = require('postcss-custom-properties');
 module.exports = {
   plugins: [
     customProperties({
+      exportTo: './vars.css',
       importFrom: {
         customProperties: {
           '--special': 'yellowgreen',
